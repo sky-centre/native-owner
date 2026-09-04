a7d371db5e51424429fa0f507251a9758f32430e
sky-centre <denxudua@gmail.com>
2026-09-05 02:38:11 +0700

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
   expo: {
     name: "Sam Zone Owner",
-    slug: "sam-chat",
+    slug: "sam-zone",
     owner: "sam-zone88",
     version: "1.0.0",
     orientation: "portrait",
@@ -33,7 +33,7 @@ export default {
       supabaseUrl: "https://jkaasjzqqhqumuzuxkmd.supabase.co",
       supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprYWFzanpxcWhxdW11enV4a21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MjczNzYsImV4cCI6MjEwNDAwMzM3Nn0.L2BpGiGsTMxlu_oNySyBmE5XvvkZ58ZcxpQeMKhKS8k",
       eas: {
-        projectId: "18fe1d81-d779-49e3-a76d-8d1556bc40b7"
+        projectId: "f220810e-9933-46cf-bdda-6b42320ac0f3"
       }
     }
   }
